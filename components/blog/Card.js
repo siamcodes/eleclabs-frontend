fe0588fb9b2fdfbfdd868accdbd0569f30c71da0
siamcodes <siamcodes@gmail.com>
2021-05-20 02:36:44 +0700

    initial

--- components/blog/Card.js
+++ components/blog/Card.js
@@ -16,7 +16,7 @@ import { withRouter } from 'next/router'; //
 
 const Card = ({ blog, router }) => {
 
-    const shareUrl = `/blogs/${blog.slug}`;
+    const shareUrl = `${DOMAIN}/blogs/${blog.slug}`;
 
     const showBlogCategories = blog =>
         blog.categories.map((c, i) => (
@@ -35,65 +35,71 @@ const Card = ({ blog, router }) => {
                 </Link> {' '}
             </>
         ));
-    
-    
+
+
 
     return (
-        <div className=" pb-4">
-            <header>
-                <Link href={`/blogs/${blog.slug}`}>
-                    <a>
-                        <h2 className="pt-2 pb-2 font-weight-bold">{blog.title}</h2>
-                    </a>
-                </Link>
-            </header>
-            <section>
-                <small className="mark">
-                    Written by{' '}
-                    <Link href={`/profile/${blog.postedBy.username}`}>
-                        <a>{blog.postedBy.name}</a>
-                    </Link>{' '}
-                    | Published {moment(blog.updatedAt).fromNow()}
-                </small>
-            </section>
-            <section>
-                {showBlogCategories(blog)}
-                {showBlogTags(blog)}
-            </section>
+        <>
+            <Head>
+                <meta charSet="UTF-8" />
+            </Head>
 
-            <div className="row">
-                <div className="col-md-4">
-                    <section>
-                        <img
-                            className="img img-fluid"
-                            style={{ maxHeight: 'auto', width: '100%' }}
-                            src={`${API}/blog/photo/${blog.slug}`}
-                            alt={blog.title}
-                        />
-                    </section>
-                </div>
-                <div className="col-md-8">
-                    <section>
-                        <div className="pb-2">{renderHTML(blog.excerpt)}</div>
-                        <Link href={`/blogs/${blog.slug}`}>
-                            <a className="btn btn-outline-info pt-2">Read more</a>
-                        </Link> {' '}
-                        <FacebookShareButton url={shareUrl} >
-                            <FacebookIcon size={32} round={true} />
-                        </FacebookShareButton>
-                        <FacebookShareCount url={shareUrl}>
-                            {shareCount => <span className="btn btn-primary">{shareCount}</span>}
-                        </FacebookShareCount>
-                        <LineShareButton url={shareUrl}>
-                            <LineIcon size={32} round={true} />
-                        </LineShareButton>
-                        <EmailShareButton url={shareUrl}>
-                            <EmailIcon size={32} round={true} />
-                        </EmailShareButton>
-                    </section>
+            <div className=" pb-4">
+                <header>
+                    <Link href={`/blogs/${blog.slug}`}>
+                        <a>
+                            <h2 className="pt-2 pb-2 font-weight-bold">{blog.title}</h2>
+                        </a>
+                    </Link>
+                </header>
+                <section>
+                    <small className="mark">
+                        Written by{' '}
+                        <Link href={`/profile/${blog.postedBy.username}`}>
+                            <a>{blog.postedBy.name}</a>
+                        </Link>{' '}
+                    | Published {moment(blog.updatedAt).fromNow()}
+                    </small>
+                </section>
+                <section>
+                    {showBlogCategories(blog)}
+                    {showBlogTags(blog)}
+                </section>
+
+                <div className="row">
+                    <div className="col-md-4">
+                        <section>
+                            <img
+                                className="img img-fluid"
+                                style={{ maxHeight: 'auto', width: '100%' }}
+                                src={`${API}/blog/photo/${blog.slug}`}
+                                alt={blog.title}
+                            />
+                        </section>
+                    </div>
+                    <div className="col-md-8">
+                        <section>
+                            <div className="pb-2">{renderHTML(blog.excerpt)}</div>
+                            <Link href={`/blogs/${blog.slug}`}>
+                                <a className="btn btn-outline-info pt-2">Read more</a>
+                            </Link> {' '}
+                            <FacebookShareButton url={shareUrl} >
+                                <FacebookIcon size={32} round={true} />
+                            </FacebookShareButton>
+                            <FacebookShareCount url={shareUrl}>
+                                {shareCount => <span className="btn btn-primary">{shareCount}</span>}
+                            </FacebookShareCount>
+                            <LineShareButton url={shareUrl}>
+                                <LineIcon size={32} round={true} />
+                            </LineShareButton>
+                            <EmailShareButton url={shareUrl}>
+                                <EmailIcon size={32} round={true} />
+                            </EmailShareButton>
+                        </section>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
