93ca6f2d2c594ca8fb3332586cb8f6bb0a11ac87
siamcodes <siamcodes@gmail.com>
2021-07-05 01:20:50 +0700

    initial

--- components/crud/BlogCreate.js
+++ components/crud/BlogCreate.js
@@ -7,8 +7,7 @@ import { getCookie, isAuth } from '../../actions/auth';
 import { getCategories } from '../../actions/category';
 import { getTags } from '../../actions/tag';
 import { createBlog } from '../../actions/blog';
-//const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
-const ReactQuill = require('react-quill');
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 //import '../../node_modules/react-quill/dist/quill.snow.css';
 import { QuillModules, QuillFormats } from '../../helpers/quill';
 
